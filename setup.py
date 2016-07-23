05314019e801a4dbbf65616846c5a7a98a85213c
mandeepbhutani <bhutanimandeep@gmail.com>
2016-07-23 16:41:12 -0500

    Removed pip install of pyqt5. Package should be installed via package manager

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 import sys
 from setuptools import setup, find_packages
 
-if sys.version_info >= (3, 5):
-    dependencies = ['pyqt5']
-else:
-    dependencies = []
-
 setup(name='Mosaic',
       version='0.0.1',
       author='Mandeep Bhutani',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=dependencies,
+      install_requires=[],
       entry_points='''
         [console_scripts]
         Mosaic=player.mosaic:main
