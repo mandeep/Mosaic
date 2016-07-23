38f471622fb2d138b50c78251516fd17f8ec05de
mandeepbhutani <bhutanimandeep@gmail.com>
2016-07-23 17:18:04 -0500

    Added open file to file menu to allow user to add media to playlist

--- player/mosaic.py
+++ player/mosaic.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent, QMediaPlaylist
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow, QApplication, QToolBar, QAction
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QToolBar,
+                             QAction, QFileDialog)
 from PyQt5.QtCore import Qt, QUrl
 
 
@@ -17,16 +18,17 @@ class MusicPlayer(QMainWindow):
         self.player = QMediaPlayer()
         self.playlist = QMediaPlaylist()
 
+        self.file_name = None
+
         self.player.setPlaylist(self.playlist)
         self.menu_controls()
         self.media_controls()
 
-        self.playlist.addMedia(QMediaContent(QUrl().fromLocalFile('')))
+        self.file_menu()
 
     def menu_controls(self):
 
         self.menu = self.menuBar()
-
         self.file = self.menu.addMenu('File')
 
     def media_controls(self):
@@ -49,6 +51,18 @@ class MusicPlayer(QMainWindow):
         self.toolbar.addAction(self.play_action)
         self.toolbar.addAction(self.pause_action)
         self.toolbar.addAction(self.stop_action)
+
+    def file_menu(self):
+        self.open_action = QAction('Open file', self)
+        self.open_action.setStatusTip('Add an audio file to an empty playlist.')
+        self.open_action.setShortcut('CTRL+O')
+        self.open_action.triggered.connect(self.open_file)
+
+        self.file.addAction(self.open_action)
+
+    def open_file(self):
+        self.file_name, ok = QFileDialog.getOpenFileName(self, 'Open file')
+        self.playlist.addMedia(QMediaContent(QUrl().fromLocalFile(self.file_name)))
 
 
 def main():
