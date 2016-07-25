6b50840f18dea18b2601bd76c3739db100b8bd75
mandeepbhutani <bhutanimandeep@gmail.com>
2016-07-24 20:13:08 -0500

    Added mutagen as dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(name='Mosaic',
       author='Mandeep Bhutani',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=[],
+      install_requires=[
+        'mutagen>=1.34'
+      ],
       entry_points='''
         [console_scripts]
         Mosaic=player.mosaic:main
