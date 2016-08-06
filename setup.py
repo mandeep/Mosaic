c55e058d2c1a98a4363b0e82dc96134c41083c91
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-05 23:06:21 -0500

    Removed include_package_date=True

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='Mosaic',
-      version='0.35',
+      version='1.0',
       author='Mandeep Bhutani',
       packages=['player', 'player.images'],
-      include_package_data=True,
       package_data={"player.images": ['*.png']},
       install_requires=[
         'mutagen>=1.34'
