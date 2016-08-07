07725149e7be4c5d95995fda4891861836a1c4a9
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-07 18:05:11 -0500

    Started building preferences dialog

--- player/configuration.py
+++ player/configuration.py
@@ -1,11 +1,39 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QDialog, QListWidget, QStackedWidget, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDialog, QGroupBox, QHBoxLayout,
+    QLabel, QListWidget, QListWidgetItem, QPushButton,
+    QStackedWidget, QVBoxLayout, QWidget)
 
 
-class ConfigurationPage(QWidget):
+class FilePage(QWidget):
 
     def __init__(self, parent=None):
-        super(ConfigurationPage, self).__init__(parent)
+        super(FilePage, self).__init__(parent)
+
+        file_config = QGroupBox("File Menu Configuration")
+
+        self.recursive_directory = QCheckBox('Recursive Open Directory '
+                                        '(open files in all subdirectories)', self)
+        self.recursive_directory.setChecked(False)
+
+        file_config_layout = QHBoxLayout()
+        file_config_layout.addWidget(self.recursive_directory)
+
+        file_config.setLayout(file_config_layout)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(file_config)
+        main_layout.addStretch(1)
+
+        self.recursive_directory.stateChanged.connect(self.recursive_directory_option)
+
+        self.setLayout(main_layout)
+
+    def recursive_directory_option(self):
+        """This setting changes the behavior of the Open Directory item in
+        the file menu. The default setting only searches for songs in the
+        selected directory. With this option checked, Open Directory will
+        also search subdirectories for songs."""
 
 
 class PreferencesDialog(QDialog):
@@ -14,3 +42,30 @@ class PreferencesDialog(QDialog):
 
         super(PreferencesDialog, self).__init__(parent)
         self.setWindowTitle('Preferences')
+        self.setFixedSize(800, 700)
+
+        self.contents = QListWidget()
+        self.pages = QStackedWidget()
+
+        self.contents.setCurrentRow(0)
+        self.pages.addWidget(FilePage())
+        self.list_items()
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.contents)
+        layout.addWidget(self.pages, 1)
+
+        self.setLayout(layout)
+
+    def list_items(self):
+        file_options = QListWidgetItem(self.contents)
+        file_options.setText('File Options')
+        file_options.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+        self.contents.currentItemChanged.connect(self.change_page)
+
+    def change_page(self, current, previous):
+        if not current:
+            current = previous
+
+        self.pages.setCurrentIndex(self.contents.row(current))
