5add9a534edde34ccf9da3bad98581b9ac84cb26
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-08 17:40:46 -0500

    Added missing comma and new package requirement

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(name='Mosaic',
       package_data={"player.images": '*.png',
                     "player": '*.toml'},
       install_requires=[
-        'mutagen==1.34'
-        'pytoml==0.1.10'
+        'mutagen==1.34',
+        'pytoml==0.1.10',
+        'appdirs==1.4.0'
       ],
       entry_points='''
         [console_scripts]
