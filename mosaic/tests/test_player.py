885e4c96389146696270d9be0f553bce25a1a648
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-13 03:03:36 -0500

    Added test for config checkbox

--- mosaic/tests/test_player.py
+++ mosaic/tests/test_player.py
@@ -1,6 +1,6 @@
-from mosaic import player
+from mosaic import player, configuration
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox
+from PyQt5.QtWidgets import QApplication, QCheckBox, QDialog, QMessageBox
 import sys
 
 app = QApplication(sys.argv)
@@ -14,6 +14,7 @@ class TestMusicPlayer:
         as the menu bar and the toolbar. Since the window would crash if these items
         did not exist or did not show, they are covered by this setup method."""
         self.music_player = player.MusicPlayer()
+        self.config_file = configuration.FileOptions()
         self.music_player.show()
 
     def test_window(self):
@@ -59,3 +60,17 @@ class TestMusicPlayer:
         qtbot.keyClick(self.music_player.help_, Qt.Key_Down)
         mock.patch.object(QMessageBox, 'exec_', return_value='')
         qtbot.keyClick(self.music_player.help_, Qt.Key_Enter)
+
+    def test_checkbox(self, qtbot, mock):
+        """Qtbot clicks on the edit menu then Qt.Key_Down highlights
+        the preferences item. The mock plugin creates a mock of the
+        QDialog window and the QCheckBox. The Qtbot's keyClick executes
+        the QDialog while its mouseClick clicks on the checkbox to test
+        for activity. """
+        qtbot.mouseClick(self.music_player.edit, Qt.LeftButton)
+        qtbot.keyClick(self.music_player.edit, Qt.Key_Down)
+        mock.patch.object(QDialog, 'exec_', return_value='')
+        mock.patch.object(QCheckBox, 'toggle', return_value='')
+        qtbot.keyClick(self.music_player.edit, Qt.Key_Enter)
+        qtbot.mouseClick(self.config_file.recursive_directory, Qt.LeftButton)
+        qtbot.mouseClick(self.config_file.recursive_directory, Qt.LeftButton)
