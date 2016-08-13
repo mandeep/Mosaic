9b8272a8e29bf7e115410a7d8e2dfc625546db41
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-12 23:00:48 -0500

    Updated package_data

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup
 setup(name='Mosaic',
       version='1.1',
       author='Mandeep Bhutani',
-      packages=['mosaic', 'mosaic.images'],
-      package_data={"mosaic.images": '*.png',
-                    "mosaic": '*.toml'},
+      packages=['mosaic', 'mosaic.images', 'mosaic.tests'],
+      package_data={'mosaic.images': ['*.png'], 'mosaic': ['*.toml']},
       install_requires=[
         'mutagen==1.34',
         'pytoml==0.1.10',
-        'appdirs==1.4.0'
+        'appdirs==1.4.0',
       ],
       entry_points='''
         [console_scripts]
