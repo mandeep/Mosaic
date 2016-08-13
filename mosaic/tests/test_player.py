1f55f6cd71ef9031af6d43b115ed4cea72c368cf
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-13 01:51:29 -0500

    Created test for preferences dialog

--- mosaic/tests/test_player.py
+++ mosaic/tests/test_player.py
@@ -1,5 +1,6 @@
 from mosaic import player
-from PyQt5.QtWidgets import (QApplication)
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QApplication, QDialog
 import sys
 
 app = QApplication(sys.argv)
@@ -8,17 +9,27 @@ app = QApplication(sys.argv)
 class TestMusicPlayer:
 
     def setup(self):
+        """Initializes the music player window for each test. Showing the window
+        has the indirect effect of testing items assigned to the main window such
+        as the menu bar and the toolbar. Since the window would crash if these items
+        did not exist or did not show, they are covered by this setup method."""
         self.music_player = player.MusicPlayer()
         self.music_player.show()
 
     def test_window(self):
+        """Asserts that the window contains the proper title as well as the
+        propeer height and width. Also asserts that the window icon appears
+        correctly."""
         assert self.music_player.windowTitle() == 'Mosaic'
         assert self.music_player.width() == 900
         assert self.music_player.height() == 963
         assert self.music_player.windowIcon().isNull() is False
 
-    def test_menubar(self):
-        assert self.music_player.menu.isVisible()
-
-    def test_toolbar(self):
-        assert self.music_player.toolbar.isVisible()
+    def test_preferences(self, qtbot, mock):
+        """Qtbot clicks on the edit menu then Qt.Key_Down highlights
+        the preferences item. The mock plugin creates a mock of the
+        QDialog window while Key_Enter executes it."""
+        qtbot.mouseClick(self.music_player.edit, Qt.LeftButton)
+        qtbot.keyClick(self.music_player.edit, Qt.Key_Down)
+        mock.patch.object(QDialog, 'exec_', return_value='')
+        qtbot.keyClick(self.music_player.edit, Qt.Key_Enter)
