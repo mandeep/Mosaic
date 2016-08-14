946aa3455ee483d9222bdc11a7335cab19ef5430
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-13 23:50:48 -0500

    New url and version number

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='mosaic-music',
-      version='1.0.0',
+      version='1.0.1',
       author='Mandeep Bhutani',
       author_email='info@mandeep.xyz',
-      url='https://mandeep.xyz',
+      url='https://github.com/mandeepbhutani/Mosaic',
       description='A cross-platform cover art focused music player.',
       packages=['mosaic', 'mosaic.images', 'mosaic.tests'],
       package_data={'mosaic.images': ['*.png'], 'mosaic': ['*.toml']},
