e4325ef121319d6efad3e7820e28d155ac21020a
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-16 04:22:40 -0500

    Increased point release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mosaic-music',
-      version='1.1.1',
+      version='1.1.2',
       author='Mandeep Bhutani',
       author_email='info@mandeep.xyz',
       url='https://github.com/mandeepbhutani/Mosaic',
