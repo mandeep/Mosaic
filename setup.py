758f49e3d56ddc50bd1faa70c34d5f63e2773e76
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-16 16:50:34 -0500

    New version point release to coincide with bux fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mosaic-music',
-      version='1.1.3',
+      version='1.1.4',
       author='Mandeep Bhutani',
       author_email='info@mandeep.xyz',
       url='https://github.com/mandeepbhutani/Mosaic',
