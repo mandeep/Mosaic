b5c32c12e2ae373c1aeec1faa7f140722c8220e4
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-24 16:08:15 -0500

    New point release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mosaic-music',
-      version='1.1.11',
+      version='1.1.12',
       author='Mandeep Bhutani',
       author_email='info@mandeep.xyz',
       url='https://github.com/mandeepbhutani/Mosaic',
