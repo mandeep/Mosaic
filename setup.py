f1948c9097837182f9d13389c9c44291f9b83143
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-27 00:11:03 -0500

    New version number that agrees with semantic versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mosaic-music',
-      version='0.1.16',
+      version='0.14.2',
       author='Mandeep Bhutani',
       author_email='info@mandeep.xyz',
       url='https://github.com/mandeepbhutani/Mosaic',
