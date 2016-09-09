764e3bd3882ef126013fb2b9de2c53f4d8b85888
mandeepbhutani <bhutanimandeep@gmail.com>
2016-09-09 11:38:34 -0500

    About dialog now shows QDialog

--- mosaic/about.py
+++ mosaic/about.py
@@ -1,9 +1,10 @@
 import pkg_resources
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QLabel, QVBoxLayout
 
 
-class AboutDialog(QMessageBox):
+class AboutDialog(QDialog):
     """Contains the necessary elements to show helpful text in a dialog."""
 
     def __init__(self, parent=None):
@@ -13,7 +14,22 @@ class AboutDialog(QMessageBox):
         self.setWindowTitle('About')
         help_icon = pkg_resources.resource_filename('mosaic.images', 'md_help.png')
         self.setWindowIcon(QIcon(help_icon))
+        self.resize(300, 200)
 
-        self.setText('Created by Mandeep Bhutani')
-        self.setInformativeText('Material design icons created by Google\n\n'
-                                'GitHub: mandeepbhutani')
+        author = QLabel('Created by Mandeep Bhutani')
+        author.setAlignment(Qt.AlignCenter)
+
+        icons = QLabel('Material design icons created by Google')
+        icons.setAlignment(Qt.AlignCenter)
+
+        github = QLabel('GitHub: mandeepbhutani')
+        github.setAlignment(Qt.AlignCenter)
+
+        self.layout = QVBoxLayout()
+        self.layout.setAlignment(Qt.AlignVCenter)
+
+        self.layout.addWidget(author)
+        self.layout.addWidget(icons)
+        self.layout.addWidget(github)
+
+        self.setLayout(self.layout)
