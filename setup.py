375beacc1a1b80e5c43c0fe16dea50ec4f5c7ae1
mandeepbhutani <mandeep@users.noreply.github.com>
2016-10-06 23:24:28 -0500

    0.18.2 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mosaic-music',
-      version='0.18.1',
+      version='0.18.2',
       author='Mandeep Bhutani',
       author_email='info@mandeep.xyz',
       url='https://github.com/mandeep/Mosaic',
