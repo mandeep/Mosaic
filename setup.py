40307e2c8dfc495f3947b489243c82a0f9e9173b
mandeep <10521687+mandeep@users.noreply.github.com>
2026-04-15 18:31:28 -0400

    Re-add setup.py for test runner

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+setup(name='mosaic-music',
+      version='0.31.3',
+      author='mandeep',
+      url='https://github.com/mandeep/Mosaic',
+      description='A cross-platform cover art focused music player.',
+      license='GPLv3+',
+      packages=['mosaic', 'mosaic.images'],
+      package_data={'mosaic.images': ['*.png'], 'mosaic': ['*.toml']},
+      install_requires=[
+        'appdirs',
+        'mutagen',
+        'toml',
+        'natsort',
+        'PySide6',
+      ],
+      extras_require={
+        'tests': [
+            'pytest',
+            'pytest-cov',
+            'pytest-qt',
+            'pytest-xvfb',
+            'pytest-mock',
+        ]
+      },
+      entry_points='''
+        [console_scripts]
+        mosaic=mosaic.player:main
+        ''',
+      classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        ]
+      )
