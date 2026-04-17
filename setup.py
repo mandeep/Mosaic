3502b019af02324431c7972add6271f0bc65e7a6
mandeep <10521687+mandeep@users.noreply.github.com>
2026-04-17 14:30:54 -0400

    Update patch version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mosaic-music',
-      version='0.35.1',
+      version='0.35.2',
       author='mandeep',
       url='https://github.com/mandeep/Mosaic',
       description='A cross-platform cover art focused music player.',
